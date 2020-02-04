88ad9512908d4bf90248ebfef160339887d1ecf6
geeta <geeta@geeta-HP-Compaq-Pro-4300-SFF-PC>
2020-02-04 12:42:41 +0530

    third commit by geeta

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,11 @@
 
 public class FirstClass {
 public static void main(String[] args) {
-	System.out.println("first class");
+	
+	int a=2;
+	for (int i = 1; i <=10; i++) {
+		int r=a*i;
+		System.out.println(r);
+	}
 }
 }
